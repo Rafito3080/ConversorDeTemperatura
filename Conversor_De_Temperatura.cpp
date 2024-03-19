bdd0d5ef9344adc713741643dab92adfb22d56e5
Rafael Costa de Lima <149844659+Rafito3080@users.noreply.github.com>
2024-03-19 18:12:50 -0300

    Update Conversor_De_Temperatura.cpp

--- Conversor_De_Temperatura.cpp
+++ Conversor_De_Temperatura.cpp
@@ -5,7 +5,7 @@ int main(){
     double temp;
     double resu1, resu2;
     int escolha;
-    cout << "Escolha qual temperatura deseja converter: \nSe desejar usar Kelvin, pressione 1. \nSe desejar usar Fahrenheit, pressione 2. \nCaso queira usar Celcius, pressione 3. \n";
+    cout << "Escolha qual unidade de temperatura deseja converter: \nSe desejar usar Kelvin (K), pressione 1. \nSe desejar usar Fahrenheit (°F), pressione 2. \nCaso queira usar Celcius (°C), pressione 3. \n";
     cin >> escolha;
     do{
         switch (escolha) {
@@ -14,49 +14,49 @@ int main(){
                 cin >> temp;
                 resu1 = temp * 9/5 - 459.67;
                 resu2 = temp - 273.15;
-                cout << temp << " K é: \nEm Fahrenheit: " << resu1 << " F° \nEm Celsius: " << resu2 << " °C";
+                cout << temp << " K é: \nEm Fahrenheit: " << resu1 << " °F \nEm Celsius: " << resu2 << " °C";
                 break;
             case 2:
                 cout << "Quantos graus Fahrenheit (°F)? \n";
                 cin >> temp;
                 resu1 = (temp - 32) / 1.8;
                 resu2 = (temp + 459.67) * 5/9;
-                cout << temp << " F° é: \nEm Celsius: " << resu1 << " C° \nEm Kelvin: " << resu2 << "K";
+                cout << temp << " °F é: \nEm Celsius: " << resu1 << " °C \nEm Kelvin: " << resu2 << "K";
                 break;
             case 3:
                 cout << "Quantos graus Celcius (°C)? \n";
                 cin >> temp;
                 resu1 = temp * 1.8 + 32;
                 resu2 = temp + 273.15;
-                cout << temp << " C° é: \nEm Fahrenheit: "<< resu1  << " F° \nEm Kelvin: " << resu2 <<" K";
+                cout << temp << " °C é: \nEm Fahrenheit: "<< resu1  << " °F \nEm Kelvin: " << resu2 <<" K";
                 break;
             default:
-                cout << "Erro ocorreu, por favor tente novamente: \nEscolha qual temperatura deseja calcular. \nSe desejar usar Kelvin, pressione 1. \nSe desejar usar Fahrenheit, pressione 2. \nCaso queira usar Celcius, pressione 3. \n";
+                cout << "Erro ocorreu, por favor tente novamente: \nEscolha qual unidade de temperatura deseja converter: \nSe desejar usar Kelvin (K), pressione 1. \nSe desejar usar Fahrenheit (°F), pressione 2. \nCaso queira usar Celcius (°C), pressione 3. \n";
                 cin >> escolha;
                 switch (escolha) {
-                            case 1:
+                case 1:
                                 cout << "Quantos graus Kelvin (K)? \n";
                                 cin >> temp;
                                 resu1 = temp * 9/5 - 459.67;
                                 resu2 = temp - 273.15;
-                                cout << temp << "K é: \nEm Fahrenheit: " << resu1 << " °F \nEm Celsius: " << resu2 << " °C";
+                                cout << temp << " K é: \nEm Fahrenheit: " << resu1 << " °F \nEm Celsius: " << resu2 << " °C";
                                 break;
                             case 2:
-                                cout << "Quantos graus Fahrenheit (F°)? \n";
+                                cout << "Quantos graus Fahrenheit (°F)? \n";
                                 cin >> temp;
                                 resu1 = (temp - 32) / 1.8;
                                 resu2 = (temp + 459.67) * 5/9;
-                                cout << temp << "°F é: \nEm Celsius: " << resu1 << " C° \nEm Kelvin: " << resu2 << "K";
+                                cout << temp << " °F é: \nEm Celsius: " << resu1 << " °C \nEm Kelvin: " << resu2 << "K";
                                 break;
                             case 3:
-                                cout << "Quantos graus Celcius(°C)? \n";
+                                cout << "Quantos graus Celcius (°C)? \n";
                                 cin >> temp;
                                 resu1 = temp * 1.8 + 32;
-                                resu2 = temp - 273.15;
+                                resu2 = temp + 273.15;
                                 cout << temp << " °C é: \nEm Fahrenheit: "<< resu1  << " °F \nEm Kelvin: " << resu2 <<" K";
                                 break;
                             default:
-                                cout << "Erro ocorreu, por favor tente novamente: \nEscolha qual temperatura deseja calcular. \nSe desejar usar Kelvin, pressione 1. \nSe desejar usar Fahrenheit, pressione 2. \nCaso queira usar Celcius, pressione 3. \n";
+                                cout << "Erro ocorreu, por favor tente novamente: \nEscolha qual unidade de temperatura deseja converter: \nSe desejar usar Kelvin (K), pressione 1. \nSe desejar usar Fahrenheit (°F), pressione 2. \nCaso queira usar Celcius (°C), pressione 3. \n";
                                 cin >> escolha;
                 }
                 break;
